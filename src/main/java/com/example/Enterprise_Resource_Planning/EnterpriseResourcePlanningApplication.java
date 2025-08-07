e0bc670d5f754a8780ac6c9774a21583c1210ab7
Omar Elrfaay <omar.tolis2004@gmail.com>
2025-08-07 16:31:15 +0300

    Enable JPA auditing in the main application class

--- src/main/java/com/example/Enterprise_Resource_Planning/EnterpriseResourcePlanningApplication.java
+++ src/main/java/com/example/Enterprise_Resource_Planning/EnterpriseResourcePlanningApplication.java
@@ -2,8 +2,10 @@ package com.example.Enterprise_Resource_Planning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EnterpriseResourcePlanningApplication {
 
 	public static void main(String[] args) {
